83f268fa18fc61a9d2dc3036d93f7498bc094e15
Yina-ship-it <84266709+Yina-ship-it@users.noreply.github.com>
2021-11-08 21:57:38 +0500

    добавлен выбор типа задачи

--- bot/pomaketu.py
+++ bot/pomaketu.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import time
 from config import *
 
@@ -12,16 +13,30 @@ def start(message):
 
 def set_description(message):
     if message.text == 'Поставить задачу':
-        bot.register_next_step_handler(message, set_summary)
-        bot.send_messge(message.chat.id,'Введите тему задачи')
+        bot.register_next_step_handler(message, set_summary_and_typeissue)
+        bot.send_message(message.chat.id,'Введите тему задачи')
     elif message.text == 'Отменить постановку задачи':
         pass
     else:
         bot.register_next_step_handler(message, set_description)
         #bot.edit_message_reply_markup(message.chat.id, reply_markup= keyboard_description())
 
-def set_summary(message):
-    pass
+def set_summary_and_typeissue(message):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button1 = types.InlineKeyboardButton(text="Задача", callback_data="task")
+    callback_button2 = types.InlineKeyboardButton(text="Ошибка", callback_data="Bug")
+    keyboard.add(callback_button1, callback_button2)
+    bot.send_message(message.chat.id,'Выберите тип задачи:', reply_markup= keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # Если сообщение из чата с ботом
+    if call.message:
+        
+        if call.data == "task":
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Вы выбрали тип Задача.")
+        elif call.data == "Bug":
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Вы выбрали тип Ошибка.")
 
 def keyboard_description():
     markup_reply = telebot.types.ReplyKeyboardMarkup(one_time_keyboard = True, resize_keyboard = True)
@@ -31,4 +46,4 @@ def keyboard_description():
     markup_reply.add(item_add_issue,item_cancel_issue)
     return markup_reply
 
-bot.polling()
+bot.polling(non_stop= True)
