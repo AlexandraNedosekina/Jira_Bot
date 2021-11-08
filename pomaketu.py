a92fd065d274ee250ee38e9d8929ab5653c2a661
Yina-ship-it <84266709+Yina-ship-it@users.noreply.github.com>
2021-11-08 19:42:46 +0500

    з

---
+++ pomaketu.py
@@ -0,0 +1,32 @@
+import telebot
+import time
+from config import *
+
+bot = telebot.TeleBot(tokenTelebot)
+
+@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'video', 'voice', 'contact'])
+def start(message):
+    bot.register_next_step_handler(message, set_description)
+    #bot.edit_message_reply_markup(message.chat.id, reply_markup= keyboard_description())
+    bot.send_message(message.chat.id,'Записываю',reply_markup= keyboard_description())
+
+def set_description(message):
+    if message.text == 'Поставить задачу':
+        pass
+    elif message.text == 'Отменить постановку задачи':
+        pass
+    else:
+        time.sleep(3)
+        bot.register_next_step_handler(message, set_description)
+        time.sleep(3)
+        #bot.edit_message_reply_markup(message.chat.id, reply_markup= keyboard_description())
+
+def keyboard_description():
+    markup_reply = telebot.types.ReplyKeyboardMarkup(one_time_keyboard = True, resize_keyboard = True)
+    item_add_issue = telebot.types.KeyboardButton(text = 'Поставить задачу')#, callback_data = 'add issue')
+    item_cancel_issue = telebot.types.KeyboardButton(text = 'Отменить постановку задачи')#, callback_data = 'cancel issue')
+
+    markup_reply.add(item_add_issue,item_cancel_issue)
+    return markup_reply
+
+bot.polling()
